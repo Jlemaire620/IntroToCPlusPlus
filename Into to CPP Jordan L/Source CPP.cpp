2607a469d39ee60439cd1ba71dd13bf1272faacf
Jordan Lemaire <s188048@students.aie.edu>
2018-08-15 17:20:09 -0500

    Add project files.

---
+++ Into to CPP Jordan L/Source CPP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string Name = "Jordan Lemaire";
+	std::string ID = "s188048";
+	std::cout << "Name:" << Name << std::endl;
+	std::cout << "ID:" << ID << std::endl;
+	
+
+	std::string First;
+	std::cout << "Enter your first name..." << std::endl;
+	std::cin >> First;
+	std::string Last;
+	std::cout << "Enter your last name..." << std::endl;
+	std::cin >> Last;
+	std::cout << "Hello there " << First << " " << Last << std::endl;
+
+	system("Pause");
+}
---
+++ Variables/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	int number;
+	number = 23;
+	std::cout << number << std::endl;
+	number = 37;
+	std::cout << number << std::endl;
+	number = 16;
+	std::cout << number << std::endl;
+	
+	int numA;
+	int numB;
+	int sum;
+	numA = 21;
+	numB = 5;
+    sum = numA + numB;
+
+	float lhs, rhs;
+	float fsum;
+	lhs = 0.2f;
+	rhs = 0.6f;
+	fsum = lhs + rhs;
+	std::cout << fsum << std::endl;
+     
+	int userInput;
+	std::cout << "Input a whole number...\n";
+	int x = 12; 
+	number = x;
+	return 0;
+    system("pause");
+
+}
