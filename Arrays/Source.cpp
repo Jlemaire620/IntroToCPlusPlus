403e96778a983d7d90d1362657b421a9de9ec742
Jordan Lemaire <s188048@students.aie.edu>
2018-09-04 15:08:57 -0500

    Homework update

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -3,29 +3,29 @@
 int main()
 {
 
-	char myFirstName[] = { 'J', 'O', 'R', 'D', 'A', 'N' };
-	int myFirstNumbers[6];
-	for (int i = 0; i < 6; i++)
+	//char myFirstName[] = { 'J', 'O', 'R', 'D', 'A', 'N' };
+	//int myFirstNumbers[6];
+	//for (int i = 0; i < 6; i++)
 
 	{
-		myFirstNumbers[i] = (int)myFirstName[i];
-		std::cout << myFirstName[i] << std::endl;
+		//myFirstNumbers[i] = (int)myFirstName[i];
+		//std::cout << myFirstName[i] << std::endl;
 	}
 
 
-	int myArray[5] = { 12,21,3,7,13 };
-	for (int i = 0; i < 5; i++)
+	//int myArray[5] = { 12,21,3,7,13 };
+	//for (int i = 0; i < 5; i++)
 	{
-		for (int j = 0; j > 5; j++)
+		//for (int j = 0; j > 5; j++)
 		{
-			if (myArray[i] < myArray[j]);
-			int temp = myArray[5];
-			myArray[i] = myArray[j];
-			myArray[j] = temp;
+			//if (myArray[i] < myArray[j]);
+			//int temp = myArray[5];
+			//myArray[i] = myArray[j];
+			//myArray[j] = temp;
 
 		}
 		//0,2,4,6,8,10,12,14,16,18
-		int Array[10];
+		/*int Array[10];
 		int counter = 0;
 		for (int i = 0; i < 20; i += 2)
 		{
@@ -39,7 +39,7 @@ int main()
 			{
 				std::cout << Arrays[i][j] << std::endl;
 			}
-		}
+		}*/
 		//Exersize - Arrays
 	}
 	   //1
@@ -76,22 +76,114 @@ int main()
 		std::cout << num5 << " " << num4 << " " << num3 << " " << num2 << " " << num1 << std::endl;
 
 		//5
-		char Entries[10];
+		int Entries[10];
+		int largest;
+		int smallest;
 		{
 			std::cout << "Enter ten numbers" << std::endl;
+			for (int i = 0; i < 10; i++)
+			std::cin >> Entries[i];
+		    largest = Entries[0];
 		}
 		for (int i = 0; i < 10; i++)
 		{
-			std::cout << Entries[i] << std::endl;
+			if (largest < Entries[i])
+				largest = Entries[i];
 		}
+		smallest = Entries[0];
+		for (int i = 0; i < 10; i++)
+		{
+			if (smallest > Entries[i])
+				smallest = Entries[i];
+		}
+		std::cout << "Largest number was.." << largest << std::endl;
+		std::cout << "Smallest number was.." << smallest << std::endl;
 
 		//6
-		int Grid[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+		int Grid[3][3] = 
 		{
-			
+			{1, 2, 3},
+		    {4, 5, 6},
+		    {7, 8, 9}
+		};
+		for (int r = 0; r < 3; r++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				std::cout << Grid[r][c] << " ";
+			}
+			std::cout << std::endl;
 		}
-		
-	
+
+		//7
+		int days[2][3] =
+		{
+			{1, 2, 3},
+			{3, 5, 7}
+		};
+		std::cout << "Row one:" << days[0][0] + days[0][1] + days[0][2] << std::endl;
+		std::cout << "Row two:" << days[1][0] + days[1][1] + days[1][2] << std::endl;
+		std::cout << "Column one:" << days[0][0] + days[1][0] << std::endl;
+		std::cout << "Column two:" << days[0][1] + days[1][1] << std::endl;
+		std::cout << "Column three:" << days[0][2] + days[1][2] << std::endl;
+
+		//8
+		int turns = 0;
+		int characterHealth[5] = { 100, 100, 100, 100, 100 };
+		int player;
+		std::cout << "Enter a number (0-4) to determine which character will be attacked" << std::endl;
+		std::cin >> player;
+		while (turns != 5)
+		if (player == 0)
+		{
+			std::cout << "Player 1 was hit!" << std::endl;
+			characterHealth[0] - 40;
+			std::cout << characterHealth[0] << std::endl;
+			std::cout << "Who was hit next?" << std::endl;
+			std::cin >> player;
+			turns++;
+		}
+		else if (player == 1)
+		{
+			std::cout << "Player 2 was hit!" << std::endl;
+			characterHealth[1] - 40;
+			std::cout << characterHealth[1] << std::endl;
+			std::cout << "Who was hit next?" << std::endl;
+			std::cin >> player;
+			turns++;
+		}
+		else if (player == 2)
+		{
+			std::cout << "Player 3 was hit!" << std::endl;
+			characterHealth[2] - 40;
+			std::cout << characterHealth[2] << std::endl;
+			std::cout << "Who was hit next?" << std::endl;
+			std::cin >> player;
+			turns++;
+		}
+		else if (player == 3)
+		{
+			std::cout << "Player 4 was hit!" << std::endl;
+			characterHealth[3] - 40;
+			std::cout << characterHealth[0] << std::endl;
+			std::cout << "Who was hit next?" << std::endl;
+			std::cin >> player;
+			turns++;
+		}
+		else if (player == 4)
+		{
+			std::cout << "Player 5 was hit!" << std::endl;
+			characterHealth[4] - 40;
+			std::cout << characterHealth[4] << std::endl;
+			std::cout << "Who was hit next?" << std::endl;
+			std::cin >> player;
+			turns++;
+		}
+		else
+		{
+			std::cout << "Invalid entry" << std::endl;
+			break;
+		}
 	system("pause");
 }
 
--- Character Arrays/Source.cpp
+++ Character Arrays/Source.cpp
@@ -61,6 +61,117 @@ int main()
 	std::cin.getline(name, 30);
 
 	//2
+	char name1[40];
+	int i;
+	std::cout << "Enter your first and last name:" << std::endl;
+	std::cin.getline(name1, 40);
+	for (i= strlen(name1); i >= 0; i--)
+	{
+		std::cout << name1[i];
+	}std::cout << std::endl;
+
+	//3
+	char name2[10];
+	char name3[10];
+	char name4[10];
+	char name5[10];
+	char name6[10];
+	int j, k, l, m, n;
+	std::cout << "Enter five names" << std::endl;
+	std::cin >> name2, name3, name4, name5, name6;
+	for (j = strlen(name2); j >= 0; j--)
+	{
+		std::cout << name2[j];
+	}std::cout << std::endl;
+
+	//4
+	std::cout << '\a';
+
+	//5
+	char username[40];
+	char password[40];
+	std::cout << "Enter a username" << std::endl;
+	std::cin >> username;
+	std::cout << "Enter an 8 or more character password that contains a capital letter,number, and a symbol" << std::endl;
+	std::cin >> password;
+	while (strlen(password) >= 8)
+    if (password[0] >= 65 && password[0] <= 90)
+	{
+		std::cout << "Capital letter requirement met." << std::endl;
+		break;
+	}
+    if (password[1] >= 65 &&  password[1] <= 90)
+	{
+		std::cout << "Capital letter requirement met." << std::endl;
+		
+	}
+    if (password[2] >= 65 && password[2] <= 90)
+	{
+		std::cout << "Capital letter requirement met." << std::endl;
+	}
+	else
+	{
+		std::cout << "Your password does not contain a capital letter!" << std::endl;
+		std::cout << "Enter your password" << std::endl;
+		std::cin >> password;
+	}
+	if (strlen(password) < 8)
+	{
+		std::cout << "Your password is too short!" << std::endl;
+		std::cout << "Enter a password" << std::endl;
+		std::cin >> password;
+	}
+	else
+	{
+		std::cout << "Valid Password" << std::endl;
+	}
 	
+
+	//6
+	char user[50];
+	char intended[] = { 'U', 's', 'r' };
+	std::cout << "Enter a username that begins with 'usr'" << std::endl;
+	std::cin >> user;
+	if (strcmp(user, intended) == 0)
+	{
+		std::cout << user << std::endl;
+	}
+	else
+	{
+		std::cout << "Invalid Entry" << std::endl;
+		std::cout << "Re-enter your username." << std::endl;
+		std::cin >> user;
+	}
+	char pass[50];
+	char intent[] = { 'P', 'a', '5', '5', 'w', '0', 'r', 'd' };
+	std::cout << "Enter the password Pa55w0rd. " << std::endl;
+	std::cin >> pass;
+	if (pass != intent)
+	{
+		std::cout << "Invalid password" << std::endl;
+	}
+	else
+	{
+		std::cout << "Password was correct" << std::endl;
+	}
+
+	//7
+	// A:  char name[4] = "John";
+	// Trying to output the name John. The problem is that the letters are not spaced. I would space them out.
+	// B:char name[32];
+    //   cout << name;
+	// Trying to output a name but no characters are assigned. I would add a name that fits the array.
+    // C: char name[7] = "Donald";
+	//    char surname[7] = "Knuth";
+	//    strcat(name, surname);
+	// Trying to copy name and surname. Surname should have size of six because it will asign garbage to the seventh slot. 
+	// I would change 7 to 6 in the array.
+	// D:char errorMsg[5] = "Stop";
+	//   errorMsg[strlen(errorMsg)] = "!";
+	//   cout << errorMsg;
+	// Trying to print out the number of letters in Stop. error message is assigned ! so it will print !.
+
+	//9
+
 	system("pause");
 }
