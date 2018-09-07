275c34413cb288db11acf47d20fc99700ef59796
Jordan Lemaire <s188048@students.aie.edu>
2018-09-07 12:25:42 -0500

    Recusrion Update

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -1,60 +1,73 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
-int print(std::string input)
-{
-	std::cout << input;
-	return 0;
-}
-int printLetters(std::string input)
-{
-	std::cout << input;
-	return 0;
-}
-bool isMultipleOfThree(int value)
-{
-	bool isMultipleOfThree = (value % 3 == 0);
-	return isMultipleOfThree;
-}
-void PrintValuesTo10()
-{
-	for (int i = 0; i <= 10; i++)
-	{
-		std::cout << i << "," << std::endl;
-	}
+//int print(std::string input)
+
+	//std::cout << input;
+	//return 0;
+
+//int printLetters(std::string input)
+
+	//std::cout << input;
+	//return 0;
+
+//bool isMultipleOfThree(int value)
+
+	//bool isMultipleOfThree = (value % 3 == 0);
+	//return isMultipleOfThree;
+
+//void PrintValuesTo10()
+
+	//for (int i = 0; i <= 10; i++)
 	
-}
-void printValuesBetween(int low, int high)
-{
-	for (int i = low + 1; i < high; i++)
-	{
-		std::cout << i << "," << std::endl;
-		printValuesBetween(low, high);
-	}  
-    system("pause");
-}
+		//std::cout << i << "," << std::endl;
+	
+	
+
+//void printValuesBetween(int low, int high)
+
+	//for (int i = low + 1; i < high; i++)
+	//{
+		//std::cout << i << "," << std::endl;
+		//printValuesBetween(low, high);
+	//}  
+    //system("pause");
+
 // create a function
 //that will take in an array and the length of the array
 //the function will return the sum of all the values in the array`
-int ArraySum(int arr[], int size)
-{
-	for (int i = 0; i < 10; i++);
-	{
-		std::cout << size;
-	}
+//int ArraySum(int arr[], int size)
+//{
+	//for (int i = 0; i < 10; i++);
+	//{
+		//std::cout << size;
+	//}
 
-}
-//create a function that will reverse the values in the arrays passed in.
+////create a function that will reverse the values in the arrays passed in.
 //You will need to pass in an array and the size of the array as arguments
 //Cannot use a second array.
-void ArraySwap(int first[], int second)
-{
+//void ArraySwap(int first[], int second)
 
+
+
+
+//5
+void CoinToss()
+{
+	int times = 0;
+	std::cout << "How many times do you want to flip the coin?" << std::endl;
+	std::cin >> times;
 }
+
+
 int main()
 {
-	int expected = 10;
-	int sizey = 4;
-	int numbs[] = { 4,3,2,1 };
-	int actual = ArraySum(numbs, sizey);
+	//int expected = 10;
+	//int sizey = 4;
+	//int numbs[] = { 4,3,2,1 };
+	//int actual = ArraySum(numbs, sizey);
 }
+
+
+
