ef013c170978a128f03808049cb8dd96da16e1b5
Jordan Lemaire <s188048@students.aie.edu>
2018-09-10 17:51:12 -0500

    Structures Update

---
+++ Structures/Source.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+
+struct Item
+{
+	std::string name;
+	int cost;
+	int quantity;
+};
+
+struct Shop
+{
+	int ShopGold = 1500;
+	Item Items[4];
+
+	void ChangePrice(std::string ItemName, int newCost)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (Items[i].name == ItemName)
+			{
+				Items[i].cost = newCost;
+			}
+		}
+	}
+	void AddStock(std::string ItemName, int quantity)
+	{
+		
+	}
+	void SellStock(std::string ItemName, int quantity)
+	{
+		
+	}
+};
+
+
+
+void main()
+{
+	Item HealthPotion;
+	HealthPotion.name = "HealthPotion";
+	HealthPotion.cost = 25;
+	HealthPotion.quantity = 5;
+	Item ManaPotion;
+	ManaPotion.name = "ManaPotion";
+	ManaPotion.cost = 30;
+	ManaPotion.quantity = 5;
+	Item Sword;
+	Sword.name = "Sword";
+	Sword.cost = 75;
+	Sword.quantity = 3;
+	Item Shield;
+	Shield.name = "Shield";
+	Shield.cost = 70;
+	Shield.quantity = 2;
+
+	Shop JordanShop;
+	JordanShop.Items[0] = HealthPotion;
+	JordanShop.Items[1] = ManaPotion;
+	JordanShop.Items[2] = Sword;
+	JordanShop.Items[3] = Shield;
+	JordanShop.ChangePrice(HealthPotion.name, 55);
+}
