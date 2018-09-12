6368b3f3f253bb8952c0ca64aa630bb1e6941c72
Jordan Lemaire <s188048@students.aie.edu>
2018-09-12 17:57:50 -0500

    I did it

---
+++ Pointers to Memory/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+//Reverse the elements that ptr prints into 
+void ReverseArray(int * ptr, int size)
+{
+	int *first;
+	int *last;
+	int temp;
+    first = ptr;
+	last = &ptr[size - 1];
+	for (int i = 0; i < size; i++)
+	{
+        temp = *first;
+	   *first = *last;
+	    *last = temp;
+	}
+}
+int main()
+{
+	//Use a pointer to initalize all values in numbs to a 1
+	/*int numbs[6];
+	int * ptr;
+	ptr = numbs;
+	ptr++;*/
+
+	int nums[3] = { 2, 4, 6 };
+	int * ptr_nums = &nums[0];
+	ReverseArray(ptr_nums, 3);
+
+	// numbs => 6,4,2
+}
+	
