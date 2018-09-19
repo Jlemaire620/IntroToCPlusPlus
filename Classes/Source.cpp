bcb53e1ba8dfe84ec8b52f4b7aea07553324535a
Jordan Lemaire <s188048@students.aie.edu>
2018-09-18 20:42:34 -0500

    Calculator update

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 //class Fighter
 //{
@@ -25,11 +26,11 @@ private:
 public:
 	bool isEmpty()
 	{
-
+		return top() == 0;
 	}
 	int top()
 	{
-
+		return values[20];
 	}
 	void pop()
 	{
@@ -45,4 +46,37 @@ int main()
 {
 	Stack stack;
 	stack.push(20);
+
+	int first;
+	int second;
+	char opp;
+	std::cout << "Enter the first number." << std::endl;
+	std::cin >> first;
+	std::cout << "Enter the second number." << std::endl;
+	std::cin >> second;
+	std::cout << "Enter the opperation." << std::endl;
+	std::cin >> opp;
+	switch (opp)
+	{
+		case '+':
+		{
+			std::cout << "Result is: " << first + second << std::endl;
+			break;
+		}
+		case '-':
+		{
+			std::cout << "Result is: " << first - second << std::endl;
+			break;
+		}
+		case '*':
+		{
+			std::cout << "Result is: " << first * second << std::endl;
+			break;
+		}
+		case '/':
+		{
+			std::cout << "Result is: " << first / second << std::endl;
+		}
+	}
+	system("pause");
 }
