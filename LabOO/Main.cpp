8b2afc4f83948920c9511a24d24aba0e6f2f70d8
Jordan Lemaire <s188048@students.aie.edu>
2018-09-28 12:50:50 -0500

    Two Heros fighting

--- LabOO/Main.cpp
+++ LabOO/Main.cpp
@@ -11,19 +11,37 @@
 int main()
 {
 	srand((unsigned)time(0));
-	 Hero *a = new Hero(100, (rand() % 100 + 1));
-	 Hero *b = new Hero(100, (rand() % 100 + 1));
+	 Hero *CaptianAmerica = new Hero(100, (rand() % 75 + 1));
+	 Hero *IronMan = new Hero(100, (rand() % 75 + 1));
 	//While the statement is true, hero a fights hero b and hero b fights hero a. 
 	 while (true)
 	 {
-		 a->Fight(*b);
-		 b->Fight(*a);
+		 CaptianAmerica->Fight(*IronMan);
+		 std::cout << "Captain America attacks IronMan" << std::endl;
+		 IronMan->Fight(*CaptianAmerica);
+		 std::cout << "IronMan attacks Capitan America" << std::endl;
+		 system("pause");
+		 bool CapAlive = CaptianAmerica->IsAlive();
+		 if(!CapAlive)
+		 {
+			 std::cout << "Capitan America was defeated." << std::endl;
+		 }
+		 bool IronAlive = IronMan->IsAlive();
+		 if (!IronAlive)
+		 {
+			 std::cout << "IronMan was defeated. " << std::endl;
+		 }
 		 //If the bool condition for both hero a and hero b are true, the fight continues. The fight ends when one of the hero bool conditions becomes false.
 		 //if a is alive or b is alive == false
 		 //break the loop
-		 bool bothAlive = a->IsAlive() && b->IsAlive();
+		 bool bothAlive = CaptianAmerica->IsAlive() && IronMan->IsAlive();
 		 if (!bothAlive)
-			 break;
+		 {
+			 std::cout << "The fight has ended." << std::endl;
+			 system("pause");
+             break;
+		 }
+			 
 	 }
 
 }
--- Morning Excersize/Source.cpp
+++ Morning Excersize/Source.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
-//Create a function that returns the sum of all numbers from 1 to n.
-//The function will have a return type of int and take in one argument of type int named n.
-int SumOfNumbers(int n)
+//Create a function with return type of integer. The function will take in two arguments.
+//The first argument is a pointer to an array and the second is the size of the array.
+//The function will return the sum of the two largest values in the array.
+
+int LargestValues(int *arr[], int size)
 {
-	int sum;
-	for (int i = 0; i < n; i++)
+	int Largest = *arr[0];
+	int SecondLargest = *arr[0];
+	for (int i = 0; i < size, i++;)
 	{
-		sum = i + n;
-    }
-	return sum;
+		if (*arr[i] > Largest)
+		{
+			Largest = *arr[i];
+		}
+	}
+	for (int in = 0; in < size, in++;)
+	{
+		if (*arr[in] > SecondLargest && *arr[in] < Largest)
+		{
+			SecondLargest = *arr[in];
+		}
+	}
+	return Largest + SecondLargest;
 }
+
 int main()
 {
-	
+	int arrays[] = { 1,2,3,4 };
+	int values = LargestValues(arrays, 4);
 }
